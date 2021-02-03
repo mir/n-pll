8020af67f8b0a11c69472e7ed6eb2bfbb386b41e
Marat Yuldashev <maratyv@gmail.com>
2021-02-03 20:31:40 +0300

    Some changes

--- nn.py
+++ nn.py
@@ -36,13 +36,11 @@ class NeuralPLLModel(nn.Module):
         super().__init__()
 
         self.network = nn.Sequential(OrderedDict([
-            ('input-1', nn.Linear(input_size, 16)),
+            ('input-1', nn.Conv1d(1, 16, 4)),
             ('activation1', nn.ReLU()),
-            ('1-2', nn.Linear(16, 8)),
-            ('activation2', nn.ReLU()),
-            ('2-3', nn.Linear(8, 4)),
-            ('activation3', nn.ReLU()),
-            ('3-output', nn.Linear(4, output_size))
+            ('2-3', nn.Conv1d(16, 1, 4)),
+            ('activation1', nn.ReLU()),
+            ('3-output', nn.Linear(2, output_size))
         ]))
         self.pll = pll.PLL()
         self.data = torch.zeros(input_size)
@@ -50,7 +48,7 @@ class NeuralPLLModel(nn.Module):
         self.labels = self.get_sin_labels()
 
     def forward(self, xb):
-        out = self.network(xb)
+        out = self.network(xb.view(1, 1, input_size))
         return out
 
     def training_step(self):
@@ -72,8 +70,8 @@ class NeuralPLLModel(nn.Module):
         return generate_sin(start_from, self.time, duration)
 
     def loss_fn(self, outputs, labels):
-        loss = (outputs[-1] - labels[-1])*(outputs[-1] - labels[-1])
-        return loss.exp()
+        loss = torch.sum((outputs - labels)*(outputs - labels))
+        return loss
 
 
 def generate_sin(start, end, duration):
@@ -87,12 +85,9 @@ def fit(iterations,
         opt_func=torch.optim.SGD):
     """Train the model using gradient descent"""
     losses = []
-    weights = []
-    grads = []
     vco_freqs = []
     vco_outs = []
     optimizer = opt_func(model.parameters(), learning_rate)
-    weights.append(get_weights(model))
     for it in range(iterations):
         percentile_count = round(iterations/10) + 1
         if it % percentile_count == 0:
@@ -102,39 +97,22 @@ def fit(iterations,
         loss.backward()
         optimizer.step()
 
-        #log gradiends
-        grads.append(get_grads(model))
-
         optimizer.zero_grad()
         model.step_end()
 
         # log data
         losses.append(loss.item())
-        weights.append(get_weights(model))
         vco_freqs.append(vco_freq/2/np.pi)
         vco_outs.append(vco_out.item())
 
 
-    return losses, weights, grads, vco_freqs, vco_outs
+    return losses, vco_freqs, vco_outs
 
-
-def get_weights(model):
-    w1 = model.network[0].weight.detach().clone()[0, :].flatten()
-    w2 = model.network[0].weight.detach().clone()[1, :].flatten()
-    w = torch.cat((w1, w2), 0)
-    return w
-
-
-def get_grads(model):
-    w1 = model.network[0].weight.grad[0, :].flatten()
-    w2 = model.network[0].weight.grad[1, :].flatten()
-    w = torch.cat((w1, w2), 0)
-    return w
 # generate data and data loaders
 
 model = NeuralPLLModel()
 
-fig, (ax1, ax2, ax3) = plt.subplots(3, 2)
+fig, (ax1, ax2) = plt.subplots(2, 2)
 fig.suptitle('NN-PLL training')
 
 # Plot reference signal
@@ -144,16 +122,13 @@ reference_signal = generate_sin(0, duration * iterations, duration)
 ax1[0].plot(reference_signal[-100:])
 
 print('Train NN')
-losses, weights, grads, vco_freqs, vco_outs = fit(iterations,
+losses, vco_freqs, vco_outs = fit(iterations,
                                          learning_rate,
                                          model)
 
-gif.save_data_animation('weights.gif', weights)
-gif.save_data_animation('grads.gif', grads)
-
 ax1[1].set_title('VCO vs Reference')
-ax1[1].plot(reference_signal[-100:])
-ax1[1].plot(vco_outs[-100:], color='r')
+ax1[1].plot(reference_signal[-200:])
+ax1[1].plot(vco_outs[-200:], color='r')
 
 ax2[0].set_title('Training losses')
 ax2[0].set_ylabel('Loss')
@@ -164,12 +139,5 @@ ax2[1].set_title('VCO frequency. Ref = {:.1f}Hz'.format(ref_frequency.item()/2/n
 ax2[1].set_xlabel('iteration')
 ax2[1].axline((0, ref_frequency.item()/2/np.pi), (1, ref_frequency.item()/2/np.pi))
 ax2[1].plot(vco_freqs[::10], color='r')
-
-ax3[0].set_title('Gradients for weights layer 1')
-ax3[0].scatter(np.arange(len(grads[0])), grads[0])
-
-ax3[1].set_title('Gradients for weights layer 1')
-ax3[1].scatter(np.arange(len(grads[100])), grads[100])
-plt.tight_layout()
 
 plt.show()
